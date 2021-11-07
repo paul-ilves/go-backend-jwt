cc3e761f772d15c1ff8bb33f48081db8b2bcf5ea
Paul Ilves <scary.lynx@gmail.com>
2021-11-08 01:26:22 +0200

    added proper db config

--- main.go
+++ main.go
@@ -4,22 +4,19 @@ import (
 	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
+	"github.com/paul-ilves/wanaku-api-go/config"
 	"github.com/paul-ilves/wanaku-api-go/routes"
 	"log"
 	"os"
 )
 
 func main() {
-	checkEnvVars()
 	app := fiber.New()
 	routes.SetupRoutes(app)
-	//repository.OpenDBConnection()
-
-	serverAddress := fmt.Sprintf("%s:%s", os.Getenv("SERVER_HOST"), os.Getenv("SERVER_PORT"))
-	log.Fatal(app.Listen(serverAddress))
+	log.Fatal(app.Listen(config.C.ServerHostPort))
 }
 
-func checkEnvVars() {
+func init() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Could not load .env file")
@@ -29,5 +26,14 @@ func checkEnvVars() {
 		if os.Getenv(envVar) == "" {
 			log.Fatalf("FATAL: environment variable %s is missing", envVar)
 		}
+	}
+
+	serverHostPort := fmt.Sprintf("%s:%s", os.Getenv("SERVER_HOST"), os.Getenv("SERVER_PORT"))
+	dbConnectionURL := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=disable",
+		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"))
+
+	config.C = &config.Config{
+		ServerHostPort:  serverHostPort,
+		DBConnectionURL: dbConnectionURL,
 	}
 }
--- repository/connection.go
+++ repository/connection.go
@@ -4,20 +4,14 @@ import (
 	"fmt"
 	_ "github.com/jackc/pgx/v4/stdlib" // load pgx driver for PostgreSQL
 	"github.com/jmoiron/sqlx"
-	"os"
+	"github.com/paul-ilves/wanaku-api-go/config"
 )
 
 var (
 	client *sqlx.DB
-
-	//maxConn, _ := strconv.Atoi(os.Getenv("DB_MAX_CONNECTIONS"))
-	//maxIdleConn, _ := strconv.Atoi(os.Getenv("DB_MAX_IDLE_CONNECTIONS"))
-	//maxLifetimeConn, _ := strconv.Atoi(os.Getenv("DB_MAX_LIFETIME_CONNECTIONS"))
-	//host := os.Getenv("DB_HOST")
 )
 
 func OpenDBConnection() {
-	// Define a new PostgreSQL connection.
 	db, err := PostgreSQLConnection()
 	if err != nil {
 		fmt.Errorf(err.Error())
@@ -37,15 +31,7 @@ func CloseDBConnection() {
 }
 
 func PostgreSQLConnection() (*sqlx.DB, error) {
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	name := os.Getenv("DB_NAME")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	url := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=disable", host, port, name, user, password)
-	// Define database connection for PostgreSQL.
-	//url := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=disable", host, port, name, user, password)
-	db, err := sqlx.Connect("pgx", url)
+	db, err := sqlx.Connect("pgx", config.C.DBConnectionURL)
 	if err != nil {
 		return nil, fmt.Errorf("error, not connected to database, %w", err)
 	}
